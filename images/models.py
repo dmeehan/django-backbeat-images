531f1e81a2f18f8d4d81a236455d7f0e282daf82
Douglas Meehan <dmeehan@gmail.com>
2011-08-25 14:09:21 -0400

    work in progress

--- images/models.py
+++ images/models.py
@@ -166,9 +166,9 @@ class RelatedImageAutoBase(ImageAutoBase):
         except:
             raise NotImplementedError
 
-    fk_field_name = property(_get_fk_field_name)
+    FK_FIELD_NAME = _get_fk_field_name(RelatedImageAutoBase)
 
-    order = PositionField(unique_for_field=self.fk_field_name)
+    order = PositionField(unique_for_field=_get_fk_field_name)
     is_main = models.BooleanField('Main image', default=False)
 
     class Meta:
